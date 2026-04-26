f40fd9e9903a882beaf100724bac72978166814c
shubhamforge <shubhamsamarth.dev@gmail.com>
2026-04-26 05:46:53 +0530

    Add Spring Boot scaffold for clinic-api service

---
+++ clinic-api/src/test/java/io/github/shubhamforge/clinic/ClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shubhamforge.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
